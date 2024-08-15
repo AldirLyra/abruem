33aa938f481bfcea304c09a7bb794f1d3af83ab2
Aldir Lyra <aldir_lyra@hotmail.com>
2024-08-15 23:00:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Abruem',
+  webDir: 'dist'
+};
+
+export default config;
