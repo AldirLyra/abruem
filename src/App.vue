f37bac49b2771f8937fe27480121c9684832985b
AldirLyra <aldir_lyra@hotmail.com>
2024-09-12 10:22:58 -0300

    feat: Melhora Estrutura de Cards da Página Principal

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,27 @@
         </ion-header>
 
         <ion-content class="custom-content">
-            <ion-img src="../resources/assets-abruem/slide_01.png"
+            <ion-img src="../resources/assets-abruem/slide_04.png"
                 class="main-image"></ion-img>
 
-            <div class="custom-divider">Informações do Evento</div>
+            <ion-card class="info-card">
+                <ion-card-content class="info-card-content">
+                    <h2>Cidade: Palmas</h2>
+                    <p>Data: 15 de Outubro de 2024</p>
+                    <p>Realização: ABRUEM</p>
+                </ion-card-content>
+            </ion-card>
 
             <ion-grid>
                 <ion-row>
-                    <ion-col size="12"
-                        size-md="6">
-                        <ion-card class="custom-card">
-                            <ion-img src="../resources/assets-abruem/elements/icon8.jpeg"
-                                class="card-image"></ion-img>
+                    <ion-col size="6"
+                        size-md="3">
+                        <ion-card class="redirect-card"
+                            style="background-image: url('../resources/assets-abruem/elements/icon8.jpeg')">
+                            <div class="icon-container">
+                                <ion-icon name="person-outline"
+                                    size="large"></ion-icon>
+                            </div>
                             <ion-card-content>
                                 <ion-button expand="block"
                                     class="custom-button blue"
@@ -28,47 +37,53 @@
                             </ion-card-content>
                         </ion-card>
                     </ion-col>
-                    <ion-col size="12"
-                        size-md="6">
-                        <ion-card class="custom-card">
-                            <ion-img src="../resources/assets-abruem/elements/icon4.jpeg"
-                                class="card-image"></ion-img>
+                    <ion-col size="6"
+                        size-md="3">
+                        <ion-card class="redirect-card"
+                            style="background-image: url('../resources/assets-abruem/elements/icon4.jpeg')">
+                            <div class="icon-container">
+                                <ion-icon name="book-outline"
+                                    size="large"></ion-icon>
+                            </div>
                             <ion-card-content>
                                 <ion-button expand="block"
                                     class="custom-button yellow"
                                     @click="openLink('https://link-para-guia')">
-                                    Guia de Apoio ao Participante
+                                    Apoio ao Participante
                                 </ion-button>
                             </ion-card-content>
                         </ion-card>
                     </ion-col>
-                </ion-row>
-
-                <ion-row>
-                    <ion-col size="12"
-                        size-md="6">
-                        <ion-card class="custom-card">
-                            <ion-img src="../resources/assets-abruem/elements/icon6.jpeg"
-                                class="card-image"></ion-img>
+                    <ion-col size="6"
+                        size-md="3">
+                        <ion-card class="redirect-card"
+                            style="background-image: url('../resources/assets-abruem/elements/icon6.jpeg')">
+                            <div class="icon-container">
+                                <ion-icon name="calendar-outline"
+                                    size="large"></ion-icon>
+                            </div>
                             <ion-card-content>
                                 <ion-button expand="block"
                                     class="custom-button green"
                                     @click="openLink('https://link-para-programacao')">
-                                    Programação
+                                    Programação do evento
                                 </ion-button>
                             </ion-card-content>
                         </ion-card>
                     </ion-col>
-                    <ion-col size="12"
-                        size-md="6">
-                        <ion-card class="custom-card">
-                            <ion-img src="../resources/assets-abruem/elements/icon5.jpeg"
-                                class="card-image"></ion-img>
+                    <ion-col size="6"
+                        size-md="3">
+                        <ion-card class="redirect-card"
+                            style="background-image: url('../resources/assets-abruem/elements/icon5.jpeg')">
+                            <div class="icon-container">
+                                <ion-icon name="image-outline"
+                                    size="large"></ion-icon>
+                            </div>
                             <ion-card-content>
                                 <ion-button expand="block"
                                     class="custom-button yellow-light"
                                     @click="openLink('https://link-para-site')">
-                                    Site do Evento
+                                    Galeria de fotos
                                 </ion-button>
                             </ion-card-content>
                         </ion-card>
@@ -80,14 +95,6 @@
                 <ion-toolbar class="footer-toolbar">
                     <div class="footer-content">
                         <p>© 2024 ABRUEM - Todos os direitos reservados.</p>
-                        <!-- <div class="footer-links">
-                            <ion-button fill="clear"
-                                class="footer-link"
-                                @click="openLink('https://link-para-termos')">Termos de Uso</ion-button>
-                            <ion-button fill="clear"
-                                class="footer-link"
-                                @click="openLink('https://link-para-privacidade')">Política de Privacidade</ion-button>
-                        </div> -->
                     </div>
                 </ion-toolbar>
             </ion-footer>
@@ -108,7 +115,6 @@ export default {
 <style scoped>
 .custom-toolbar {
     --background: #324ca6;
-    /* Cor azul */
 }
 
 .header-title {
@@ -120,39 +126,117 @@ export default {
 
 .custom-content {
     --background: #f29f05;
-    /* Cor laranja clara */
 }
 
 .main-image {
     width: 100%;
     height: auto;
-    margin-bottom: 20px;
+    margin-bottom: 0;
+    /* Colar o card ao banner */
 }
 
-.custom-card {
-    text-align: center;
+.info-card {
+    margin-top: -2px;
+    border: 2px solid #f29f05;
     border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    margin-bottom: 20px;
-    transition: transform 0.2s;
-    --background: #ffffff;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+    --background: #324ca6;
+    color: white;
 }
 
-.custom-card:hover {
+.info-card-content {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+}
+
+.info-card h2 {
+    font-size: 18px;
+    font-weight: bold;
+    margin: 0;
+}
+
+.info-card p {
+    font-size: 16px;
+    margin: 5px 0;
+}
+
+/* Custom Divider */
+.custom-divider {
+    text-align: center;
+    margin: 20px 0;
+    font-size: 20px;
+    font-weight: bold;
+    color: #324ca6;
+}
+
+/* Estilização dos cards de redirecionamento */
+.redirect-card {
+    position: relative;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: overlay;
+    background-color: rgba(0, 0, 0, 0.5);
+    /* Tornar o fundo opaco/transparente */
+    border-radius: 10px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
+    /* Sombra mais forte */
+    color: white;
+    transition: transform 0.2s;
+    padding: 20px;
+}
+
+.redirect-card:hover {
     transform: scale(1.05);
 }
 
-.card-image {
-    width: 100px;
-    height: 100px;
-    margin: 20px auto;
-    display: block;
+.icon-container {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    background-color: rgba(0, 0, 0, 0.5);
+    /* Fundo semitransparente para destacar o ícone */
+    padding: 10px;
+    border-radius: 50%;
 }
 
-.custom-button {
-    margin-top: 10px;
+.ion-icon {
+    color: white;
+    font-size: 32px;
+}
+
+ion-card-content {
+    display: flex;
+    justify-content: center;
+    /* Centralizar o conteúdo horizontalmente */
+    align-items: flex-end;
+    /* Garantir que o botão fique alinhado ao final do card */
+    padding-bottom: 20px;
+    /* Adicionar um espaçamento no final do card */
+    height: 100%;
+    /* Certificar que o card preencha toda a altura disponível */
+    position: relative;
+}
+
+ion-button {
     font-weight: bold;
-    --border-radius: 8px;
+    white-space: normal;
+    text-align: center;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 120px;
+    /* Largura fixa para consistência */
+    height: 44px;
+    /* Definir uma altura consistente para todos os botões */
+    margin: 0 auto;
+    /* Centralizar o botão */
 }
 
 .custom-button.blue {
@@ -162,7 +246,7 @@ export default {
 
 .custom-button.yellow {
     --background: #edd662;
-    --color: black;
+    --color: white;
 }
 
 .custom-button.green {
@@ -172,52 +256,9 @@ export default {
 
 .custom-button.yellow-light {
     --background: #f2cb05;
-    --color: black;
+    --color: white;
 }
 
-.custom-divider {
-    text-align: center;
-    margin: 20px 0;
-    font-size: 20px;
-    font-weight: bold;
-    color: #324ca6;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.custom-divider:before,
-.custom-divider:after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    width: 30%;
-    height: 2px;
-    background-color: #324ca6;
-}
-
-.custom-divider:before {
-    left: 5%;
-}
-
-.custom-divider:after {
-    right: 5%;
-}
-
-@media (max-width: 600px) {
-
-    .custom-divider:before,
-    .custom-divider:after {
-        width: 20%;
-    }
-
-    .custom-divider {
-        font-size: 18px;
-    }
-}
-
-/* Estilos do Footer */
 .custom-footer {
     --background: #324ca6;
     margin-top: 20px;
@@ -231,23 +272,5 @@ export default {
 .footer-content {
     text-align: center;
     color: white;
-}
-
-.footer-links {
-    margin-top: 10px;
-}
-
-.footer-link {
-    color: white;
-    font-size: 14px;
-    margin: 5px 10px;
-    --background: rgba(255, 255, 255, 0.2);
-    --color: white;
-    --border-radius: 5px;
-    padding: 5px 10px;
-}
-
-.footer-link:hover {
-    --background: rgba(255, 255, 255, 0.3);
 }
 </style>
