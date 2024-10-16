b435c62a5e87bcfa074f64569c10140e39e74f18
Aldir Lyra <aldir_lyra@hotmail.com>
2024-10-16 18:00:48 -0300

    feat: Inclui ícone do APP

---
+++ android/app/src/main/java/com/abruem/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.abruem.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
+    appId: 'com.abruem.app',
     appName: 'Abruem 2024',
     webDir: 'dist'
 };
